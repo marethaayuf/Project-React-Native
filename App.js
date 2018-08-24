33d8213b9305901f3dbda28970dc07f726258583
Maretha <marethaayuf@gmail.com>
2018-08-24 15:18:56 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama : Caesariyanti Maretha Ayu Fadhilah</Text>
+        <Text>Kelas : XI RPL1 </Text>
+        <Text>No Absen : 11</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
